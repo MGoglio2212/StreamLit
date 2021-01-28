404907f5989d78fcceaa401ac0acb81faed7bc38
MGoglio2212 <73357232+MGoglio2212@users.noreply.github.com>
2021-01-28 10:30:53 +0100

    Add files via upload

--- App.py
+++ App.py
@@ -273,7 +273,7 @@ if uploaded_file is not None:
             Durata = "fino 31/12/2022"
         if filename == "CTE_1002189.pdf":
             Price = ""
-            TipoPrezzo = "PSV + 0,45"
+            TipoPrezzo = "VARIABILE PSV + 0,45"
         if filename == "CE_POWER_BASE_LSIC.pdf":
             PrezzoVerde = "2 MESE"
             Durata = "fino 31/12/2021"
@@ -294,7 +294,7 @@ if uploaded_file is not None:
         
         if Price != "":
             st.markdown("<h3 style='text-align: left; color: black;'>Prezzo unitario materia prima:</h1>", unsafe_allow_html=True)
-            st.markdown("<h2 style='text-align: left; color: black;'>(se variabile prezzo riferimento riportato)</h1>", unsafe_allow_html=True)
+            st.markdown("<h2 style='text-align: left; color: black;'>(non scontato - se variabile prezzo riferimento riportato)</h1>", unsafe_allow_html=True)
             st.write(Price) 
                 
     
